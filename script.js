4b211be8e1c4e974d341f723805ca93e7d5f860d
Baishali Deb Kanunjna <108573418+baishalidebk13@users.noreply.github.com>
2025-03-23 19:13:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// header scrolling effect
+// Header scrolling effect
 $(window).on('scroll', function(){
-	if($(window).scrollTop()){
-      $('header').addClass('nav-show');
-		  
-	} 
-	else{
-		$('header').removeClass('nav-show');
-	}
-	   
-})
+    if($(window).scrollTop()){
+        $('header').addClass('nav-show');
+    } else {
+        $('header').removeClass('nav-show');
+    }
+});
 
-//dynamic footer content
+// Dynamic footer content
 var year = new Date().getFullYear();
 var date = `©️ BaishaliPortfolio ${year} . All Rights Reserved`;
 document.getElementsByTagName('footer')[0].innerHTML = date;
+
+// Toggle Black/White Mode function
+function toggleBWMode() {
+    document.body.classList.toggle('bw-mode');
+}
